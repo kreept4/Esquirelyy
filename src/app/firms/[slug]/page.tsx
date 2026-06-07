183c69a8d710f3e7be17036f40baec2b6dfaa430
kreept4 <kreept4@gmail.com>
2026-06-07 12:57:30 -0700

    fix: add use client to firm profile page

--- src/app/firms/[slug]/page.tsx
+++ src/app/firms/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+'use client'
+  import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
